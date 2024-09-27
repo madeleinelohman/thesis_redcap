13272fc7ea473605d2a561c867b43e2f96f9fb31
madeleinelohman <38639678+madeleinelohman@users.noreply.github.com>
2024-09-27 11:56:04 -0700

    Add files via upload

--- pca_ppr.R
+++ pca_ppr.R
@@ -20,7 +20,6 @@ source("ppr_n_clust.R")
 
 load("map_dat.RData")
 load("combined_ppr.RData")
-load("banding_data/band_locs.RData")
 
 ### Prep data
 all.new <- st_drop_geometry(all)
@@ -28,16 +27,39 @@ for(i in 1:ncol(all.new)){
   all.new[,i] <- as.numeric(unlist(all.new[,i]))
 }
 colnames(all.new)
-#all.new <- all.new[,-c(1,7,10, 17)]
-all.new <- all.new[,-c(1,3:6, 8, 17)]
-colnames(all.new)
 
+need <- cor(all.new)
+corrplot(need)
+
+all.new <- all.new[,-c(1,3:8, 17)]
+# all.new <- all.new[,-c(1, 8, 10, 17)]
+# all.new <- all.new[,-c(1, 8:10, 17)]
+# all.new <- all.new[,-c(9)]
+
+
+states <- unique(counties.ppr$STATE_NAME)
+counties.ppr$state_id <- NA
+for(i in 1:length(states)){
+  counties.ppr$state_id[which(counties.ppr$STATE_NAME == states[i])] <- i
+}
+
+counties.ppr$country <- "US"
+can.states <- c("Alberta", "Manitoba", "Saskatchewan")
+counties.ppr$country[which(counties.ppr$STATE_NAME%in%can.states)] <- "CAN"
+
+counties.ppr$country_id <- 1
+counties.ppr$country_id[which(counties.ppr$STATE_NAME%in%can.states)] <- 2
+
+states2 <- matrix(0, nrow(counties.ppr), length(states))
+for(i in 1:length(states)){
+  states2[which(counties.ppr$STATE_NAME == states[i]), i] <- 1
+}
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Run PCA
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ### PCA
-pc.res <- prcomp(~., data=all.new, scale.=TRUE, center=T)
+pc.res <- prcomp(~., data=all.new, scale.=T, center=T)
 
 ### Look at summary
 summ <- summary(pc.res)
@@ -75,23 +97,29 @@ fviz_eig(pc.res, addlabels = TRUE) # Eigenvalues (What explains the most varianc
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Run Redcap
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### How to bound the clusters
+counties.ppr$area <- as.numeric(st_area(counties.ppr))
+bound_vals <- counties.ppr["area"]
+min_bound <- 1*10^10
+
 ### How many clusters to use?
-min.clust = 20
-max.clust = 50
-pc.want = 1:6
+min.clust = 30
+max.clust = 120
+pc.want = 1:5
 ## Scoring
 # "S_Dbw"
 # "Calinski_Harabasz"
 # "Davies_Bouldin"
+# "SD_Scat"
 
-test <- num.clust(counties.ppr, pc.res, min.clust, max.clust, pc.want, 
-                     "Davies_Bouldin")
+test <- num.clust(counties.ppr, pc.res, min.clust, max.clust, pc.want,
+                  "SD_Scat", bound_vals, min_bound, states2)
 print(test["rbsste.plot"])
 print(test["index.plot"])
 n.clust = test$want
 
 
-### Put first 3 PCs into data frame and combine with geographic data
+### Put first # of important PCs into data frame and combine with geographic data
 dat <- pc.res$x[,pc.want]
 dat <- cbind(counties.ppr, dat)
 
@@ -100,9 +128,11 @@ rook_w <- rook_weights(dat)
 
 ### Specific data values we want
 data <- dat[,grep("PC",colnames(dat))] 
+data <- cbind(states2, data)
+data <- st_drop_geometry(data)
 
 ### Run Redcap!!
-cr <- redcap(n.clust, rook_w, data, "fullorder-completelinkage") 
+cr <- redcap(n.clust, rook_w, data, "fullorder-completelinkage", scale_method="raw") 
 cr
 
 #~~~~~~~~~~~~~~~~~~
@@ -110,8 +140,7 @@ cr
 #~~~~~~~~~~~~~~~~~~
 ### Get ready to plot
 # Put clusters into data frame 
-dat$pred <- cr$Clusters
-dat$pred <- as.factor(dat$pred)
+dat$pred <- as.factor(cr$Clusters)
 
 # Put together counties in the same cluster
 new <- dat %>%
@@ -132,17 +161,3 @@ ggplot(new) +
   scale_fill_manual(values=cols, aesthetics="fill") +
   theme_classic() +
   theme(legend.position="none")
-
-# ggplot(new) +
-#   geom_sf(aes(fill=pred))+
-#   xlim(st_bbox(grd2)[c(1,3)]) + ylim(st_bbox(grd2)[c(2, 4)]) +
-#   geom_sf(data=ppr_states, fill=NA, color="grey50", size=0.25) +
-#   geom_sf(data=rels.n, size=0.85) +
-#   scale_fill_manual(values=cols, aesthetics="fill") +
-#   theme_classic() +
-#   theme(legend.position="none")
-
-
-save.image(file="pca_res.RData")
-
-
--- ppr_n_clust.R
+++ ppr_n_clust.R
@@ -1,10 +1,11 @@
-# min.clust=3
-# max.clust=40
-# index="Calinski_Harabasz"
-# pc.want=1:2
-# df=counties.ppr
+min.clust=30
+max.clust=100
+index="S_Dbw"
+pc.want=1:5
+df=counties.ppr
 
-num.clust <- function(df, pc.res, min.clust, max.clust, pc.want, index){
+num.clust <- function(df, pc.res, min.clust, max.clust, pc.want, index, bound_vals, 
+                      bound_min, states2){
   
   ### Put desired PCs into data frame and combine with geographic data
   pcs <- paste0("PC", pc.want)
@@ -13,20 +14,22 @@ num.clust <- function(df, pc.res, min.clust, max.clust, pc.want, index){
   dat <- dat[,c(pcs, "geometry")]
   
   ### Calculate neighborhood matrices
-  rook_w <- rook_weights(dat) 
+  rook_w <- queen_weights(dat) 
   
   ### Specific data values we want
   data <- dat[pcs] %>%
     st_drop_geometry()
-  data.eval <- as.matrix(data)
+  data.eval <- cbind(states2, data)
   
   ### Create an empty dataframe to figure out ideal number of clusters
   eval <- data.frame(clusters=min.clust:max.clust, RBTSSE=NA, Index=NA)
   
   for(i in min.clust:max.clust){
-    cr_q <- redcap(i, rook_w, data, "fullorder-completelinkage", cpu_threads=1)
+    cr_q <- redcap(i, rook_w, data.eval, "fullorder-completelinkage", 
+                   scale_method="raw",
+                   bound_vals, min_bound)
     eval$RBTSSE[i-(min.clust-1)] <- cr_q$`The ratio of between to total sum of squares`
-    eval$Index[i-(min.clust-1)] <- unlist(intCriteria(data.eval, as.vector(as.integer(cr_q$Clusters)), crit=index))
+    eval$Index[i-(min.clust-1)] <- unlist(intCriteria(as.matrix(data.eval), as.vector(as.integer(cr_q$Clusters)), crit=index))
   }
   
   RBTSSE <- ggplot(eval, aes(x=clusters, y=RBTSSE)) +
